1a8c7dbff98192b939ecf72e7032d4e8a805f9d2
Mr.Steinfeger <bao66@hotmail.com>
2026-04-22 10:51:22 +0200

    Product hinzugefüugt und controllert

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class MyController {
-    @GetMapping("/api/test")
-    public String getTest() {
-        return "Milestone 1 erfolgreich!";
+
+    @GetMapping("/api/products")
+    public List<Product> getProducts() {
+        return List.of(
+                new Product("1", "Laptop", 500.00),
+                new Product("2", "Kopfhörer", 150.00)
+        );
     }
 }
