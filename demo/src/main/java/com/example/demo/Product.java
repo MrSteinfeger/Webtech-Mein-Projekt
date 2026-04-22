db3b28f94f2ebaee8d6c92de33be662a70c93ed5
Mr.Steinfeger <bao66@hotmail.com>
2026-04-22 10:52:00 +0200

    Product hinzugefüugt und controllert

--- demo/src/main/java/com/example/demo/Product.java
+++ demo/src/main/java/com/example/demo/Product.java
@@ -5,14 +5,14 @@ public class Product {
     private String name;
     private double price;
 
-    public Product(String id, String name, double price){
-
+    public Product(String id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
+
+    // Getter & Setter (wichtig für Spring!)
     public String getId() { return id; }
     public String getName() { return name; }
     public double getPrice() { return price; }
-
 }
