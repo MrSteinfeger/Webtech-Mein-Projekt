fbe76af9976cdf6e6100ed61ec2c57c41b56c22c
Mr.Steinfeger <bao66@hotmail.com>
2026-05-24 22:25:07 +0200

    cross origin hinhzugefügt

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class MyController {
 
     @GetMapping("/api/products")
